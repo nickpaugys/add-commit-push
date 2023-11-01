a86751cdb7c63874d868757af9bc80459bc40ff4
Nick Paugys <nicpaugys@gmail.com>
2023-11-01 11:37:13 -0500

    update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -2,9 +2,15 @@ import os
 
 print('git status')
 os.system('git status')
-print('\ngit add -A')
-os.system('git add -A')
-print('\ngit commit -m "update files"')
-os.system('git commit -m "update files"')
-print('\ngit push')
-os.system('git push')
+
+MESSAGE = input("\nAre you sure that you'd like to commit and push changes?").strip
+if MESSAGE == "yes" or "y":
+    print('\ngit add -A')
+    os.system('git add -A')
+    print('\ngit commit -m "update files"')
+    os.system('git commit -m "update files"')
+    print('\ngit push')
+    os.system('git push')
+
+elif MESSAGE == "no":
+    exit
