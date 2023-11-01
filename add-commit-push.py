991e8a5d13fa7accfa8996940e54392b7f121758
Nick Paugys <nicpaugys@gmail.com>
2023-11-01 11:22:04 -0500

    update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,1 +1,10 @@
-print("Starting add-commit-push")
+import os
+
+print('git status')
+os.system('git status')
+print('\ngit add -A')
+os.system('git add -A')
+print('\ngit commit -m "update files"')
+os.system('git commit -m "update files"')
+print('\ngit push')
+os.system('git push')
