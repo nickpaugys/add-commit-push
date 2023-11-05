d492511b832233df852f673570d935381b20b9ff
Nick Paugys <nicpaugys@gmail.com>
2023-11-05 11:52:57 -0600

    update files

--- add-commit-push.py
+++ add-commit-push.py
@@ -1,16 +1,39 @@
 import os
+import sys
+
+commitCommand = '\ngit commit -m "update files"'
+if len(sys.argv) == 3:
+    if sys.argv[1] == '-m':
+        # if there are 3 arguments and the second one is -m, use the third as a commit message
+        commitCommand = '\ngit commit -m "' + sys.argv[2] + '"'
 
 print('git status')
 os.system('git status')
 
-MESSAGE = input("\nAre you sure that you'd like to commit and push changes?").strip
-if MESSAGE == "yes" or "y":
+force = False
+for x in range(len(sys.argv)):
+    print(x)
+    if sys.argv[x] == '-f':
+        print('force = True')
+        force = True
+
+def addcommitpush():
     print('\ngit add -A')
     os.system('git add -A')
-    print('\ngit commit -m "update files"')
-    os.system('git commit -m "update files"')
+    print(commitCommand)
+    os.system(commitCommand)
     print('\ngit push')
     os.system('git push')
 
-elif MESSAGE == "no":
-    exit()
+
+if force == False:
+    MESSAGE = input("\nAre you sure that you'd like to commit and push changes? ").strip().lower()
+    if MESSAGE == "yes" or "y":
+        addcommitpush()
+
+elif force == True:
+    addcommitpush()
+
+else:
+    print('\nYou have chosen to terminate the ACP session.')
+    quit()
